587d096d40bbcc32959acdab76ee92bbce4cf7d0
Krister Jansson <krustur@gmail.com>
2020-08-31 23:02:27 +0200

    tundra lua file formatted

--- tundra.lua
+++ tundra.lua
@@ -1,58 +1,62 @@
 local common = {
-  Env = {
-    CPPPATH = { 
-        "include",
-		"D:/SDK/glfw-3.3.2.bin.WIN64/include",
-    },
-	LIBPATH = {
-	 "D:/SDK/glfw-3.3.2.bin.WIN64/lib-vc2019",
-	},
-    CXXOPTS = {
-	  { "/EHsc"; Config = "*-msvc-*" },
-      { "/Zi"; Config = "*-msvc-debug" },
-      { "/MDd" },	  
-    },
-    CPPDEFS = {
-      { "_DEBUG"; Config = "*-*-debug" },
-      { "NDEBUG"; Config = "*-*-release" },
-      { "_CRT_SECURE_NO_WARNINGS"; Config = "*-msvc-*" },
-    },
-	PROGOPTS = {
-	  { "/NODEFAULTLIB:LIBCMT" },
-	  { "/NODEFAULTLIB:MSVCRT" },
-	}
+    Env = {
+        CPPPATH = {
+            "include",
+            "D:/SDK/glfw-3.3.2.bin.WIN64/include"
+        },
+        LIBPATH = {
+            "D:/SDK/glfw-3.3.2.bin.WIN64/lib-vc2019"
+        },
+        CXXOPTS = {
+            {"/EHsc", Config = "*-msvc-*"},
+            {"/Zi", Config = "*-msvc-debug"},
+            {"/MDd"}
+        },
+        CPPDEFS = {
+            {"_DEBUG", Config = "*-*-debug"},
+            {"NDEBUG", Config = "*-*-release"},
+            {
+                "_CRT_SECURE_NO_WARNINGS",
+                Config = "*-msvc-*"
+            }
+        },
+        PROGOPTS = {
+            {"/NODEFAULTLIB:LIBCMT"},
+            {"/NODEFAULTLIB:MSVCRT"}
+        }
 
-  },
-  ReplaceEnv = {
-  },
+    },
+    ReplaceEnv = {}
 }
 
 Build {
-  Units = function () 
-    local deluxe = Program {
-      Name = "eengine3",
-	  SourceDir = "src",
-      Sources = {
-        "main.cpp",
-        "glad.c"
-      },
-	  Libs = { 
-		"opengl32.lib",
-		"glfw3.lib",
-		"user32.lib",		
-		"Shell32.lib",
-		"Gdi32.lib",
-	  },
-    }
-    Default(deluxe)    
-  end,
+    Units = function()
+        local deluxe = Program {
+            Name = "eengine3",
+            SourceDir = "src",
+            Sources = {"main.cpp", "glad/glad.c"},
+            Libs = {
+                "opengl32.lib",
+                "glfw3.lib",
+                "user32.lib",
+                "Shell32.lib",
+                "Gdi32.lib"
+            }
+        }
+        Default(deluxe)
+    end,
 
-  Configs = {
-    Config {
-      Name = "win64-msvc",
-      DefaultOnHost = "windows",
-      Inherit = common,
-      Tools = { { "msvc-vs2019"; TargetArch = "x64" }, },
-    },    
-  },
+    Configs = {
+        Config {
+            Name = "win64-msvc",
+            DefaultOnHost = "windows",
+            Inherit = common,
+            Tools = {
+                {
+                    "msvc-vs2019",
+                    TargetArch = "x64"
+                }
+            }
+        }
+    }
 }
