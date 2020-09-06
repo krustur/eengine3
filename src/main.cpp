4ed2fff181049f396a38e31d2a015ed49cc1175e
Krister Jansson <krustur@gmail.com>
2020-09-06 11:33:54 +0200

    transformations - updated shader class

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,11 @@ void processInput(GLFWwindow *window)
 
 int main()
 {
-
-	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-	vec = trans * vec;
-	std::cout << vec.x << " " << vec.y << " " << vec.z << " " << std::endl;
+	// glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	// glm::mat4 trans = glm::mat4(1.0f);
+	// trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	// vec = trans * vec;
+	// std::cout << vec.x << " " << vec.y << " " << vec.z << " " << std::endl;
 
 
 	// glfw: initialize and configure
@@ -184,7 +183,11 @@ int main()
 
 		// draw our first triangle
 	
-
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 		// float timeValue = glfwGetTime();
 		// float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 		// int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
