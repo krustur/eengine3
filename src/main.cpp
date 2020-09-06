9e5980c06894fb7390cbd1536d95efc58f3b32a5
Krister Jansson <krustur@gmail.com>
2020-09-06 22:32:06 +0200

    circling camera

--- src/main.cpp
+++ src/main.cpp
@@ -205,10 +205,28 @@ int main()
 		// activate shader
         ourShader.use();
 
+		// Camera position
+		// glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f); 
+		// glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		// glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+		// glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); 
+		// glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+		// glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+		// glm::mat4 view;
+		// view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), 
+		// 				   glm::vec3(0.0f, 0.0f, 0.0f), 
+		// 				   glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+		const float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		glm::mat4 view;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0)); 
+
 		// create transformations
-		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection;
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
       	ourShader.setMat4("projection", projection); // note: set it outside the main loop only once.
         ourShader.setMat4("view", view);
